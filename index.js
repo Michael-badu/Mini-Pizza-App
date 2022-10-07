68cb7bbf5779c82593170034d923f1bcefe205ba
Oladipo Oluwasegun <oladipooluwasegun96@gmail.com>
2022-10-07 21:08:28 +0100

    userModel and orderModel

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const orderModel = require('./orderModel');
+const userModel = require('./userModel');
 const moment = require('moment')
 
 const PORT = 3334
@@ -31,6 +32,51 @@ app.post('/order', async (req, res) => {
     })
 
     return res.json({status: true, order })
+})
+
+app.get('/order/:orderId', async (req, res) => {
+    const { orderId } = req.params;
+    const order = await orderModel.findById(orderId)
+
+    if (!order) {
+        return res.status(404).json({status:false, order: null})
+    }
+
+    return res.json({status: true, order})
+})
+
+app.get('/orders', async (req, res) => {
+    const orders = await orderModel.find()
+
+    return res.json({status: true, orders})
+})
+
+app.patch('/order/:id', async (req, res) => {
+    const { id } = req.params;
+    const { state } = req.body;
+
+    const order = await orderModel.findById(id)
+
+    if (!order) {
+        return res.status(404).json({ status: false, order: null })
+    }
+
+    if (state < order.state) {
+        return res.status(422).json({ status: false, order: null, message: 'Invalid operation'})
+    }
+    order.state = state;
+
+    await order.save()
+    
+    return res.json({ status: true, order})
+})
+
+app.delete('/order/:id', async (req, res) => {
+    const { id } = req.params;
+
+    const order = await orderModel.deleteOne({_id: id})
+
+    return res.json({ status: true, order})
 })
 
 
